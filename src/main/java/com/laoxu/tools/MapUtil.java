99293fa63fdae59b5387cbce7d2d7543d4627536
xdcloas113 <loasloas13>
2019-06-24 18:02:05 +0800

    整理工具

--- src/main/java/com/laoxu/tools/MapUtil.java
+++ src/main/java/com/laoxu/tools/MapUtil.java
@@ -1,14 +1,25 @@
 package com.laoxu.tools;
 
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
- * 所在包：com.laoxu.tools
+ * 所在包：com.scmofit.gifm.common.commonJson
  * 文件名：
- * 项目：common
- * 功能描述： 去除Map 空值
+ * 版权：四川兴财信息产业发展有限公司.
+ * 项目：gifm
+ * 功能描述：Map处理空    key value
  * 修改人：xdc
- * 创建时间：2019-02-22 17:43
+ * 创建时间：2019-02-22 17:41
  */
 public class MapUtil {
     /**
@@ -50,19 +61,11 @@ public class MapUtil {
     /**
      * 移除map中的空值
      *
-     * Iterator 是工作在一个独立的线程中，并且拥有一个 mutex 锁。
-     * Iterator 被创建之后会建立一个指向原来对象的单链索引表，当原来的对象数量发生变化时，这个索引表的内容不会同步改变，
-     * 所以当索引指针往后移动的时候就找不到要迭代的对象，所以按照 fail-fast 原则 Iterator 会马上抛出 java.util.ConcurrentModificationException 异常。
-     * 所以 Iterator 在工作的时候是不允许被迭代的对象被改变的。
-     * 但你可以使用 Iterator 本身的方法 remove() 来删除对象， Iterator.remove() 方法会在删除当前迭代对象的同时维护索引的一致性。
-     * @param obj
-     * @param iterator
      */
     private static void remove(Object obj,Iterator iterator){
         if(obj instanceof String){
             String str = (String)obj;
-            if(isEmpty(str)){  //过滤掉为null和""的值 主函数输出结果map：{2=BB, 1=AA, 5=CC, 8=  }
-//            if("".equals(str.trim())){  //过滤掉为null、""和" "的值 主函数输出结果map：{2=BB, 1=AA, 5=CC}
+            if(isEmpty(str)){
                 iterator.remove();
             }
 
@@ -94,16 +97,139 @@ public class MapUtil {
         return obj == null || obj.toString().length() == 0;
     }
 
-    public static void main(String[] args) {
-        Map map = new HashMap();
-        map.put(1, "AA");
-        map.put("2", "BB");
-        map.put("5", "CC");
-        map.put("6",null);
-        map.put("7", "");
-        map.put("8", "  ");
-        System.out.println(map);//输出结果：{2=BB, 1=AA, 7=, 6=null, 5=CC, 8=  }
-        removeNullEntry(map);
-        System.out.println(map);
+
+    /**
+     *  map 转成对象
+     * @param clazz 对象的class
+     * @param map map
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
+    public static <T>T convertMap(Class<T> clazz, Map map) throws Exception {
+        BeanInfo beanInfo = Introspector.getBeanInfo(clazz); // 获取类属性
+        T obj = clazz.newInstance();
+        // 给 JavaBean 对象的属性赋值
+        PropertyDescriptor[] propertyDescriptors =  beanInfo.getPropertyDescriptors();
+        for (int i = 0; i< propertyDescriptors.length; i++) {
+            PropertyDescriptor descriptor = propertyDescriptors[i];
+            String propertyName = descriptor.getName();
+
+            if (map.containsKey(propertyName)) {
+                // 下面一句可以 try 起来，这样当一个属性赋值失败的时候就不会影响其他属性赋值。
+                Object value = map.get(propertyName);
+
+                Object[] args = new Object[1];
+                args[0] = value;
+                Field privateField = getPrivateField(propertyName, clazz);
+                if (privateField == null) {
+                }
+                privateField.setAccessible(true);
+                String type = privateField.getGenericType().toString();
+                if (type.equals("class java.lang.String")) {
+                    privateField.set(obj, value);
+                } else if (type.equals("class java.lang.Boolean")) {
+                    privateField.set(obj, Boolean.parseBoolean(String.valueOf(value)));
+                } else if (type.equals("class java.lang.Long")) {
+                    privateField.set(obj, Long.parseLong(String.valueOf(value)));
+                } else if (type.equals("class java.lang.Integer")) {
+                    privateField.set(obj, Integer.parseInt(String.valueOf(value)));
+                } else if (type.equals("class java.lang.Double")) {
+                    privateField.set(obj,Double.parseDouble(String.valueOf(value)));
+                } else if (type.equals("class java.lang.Float")) {
+                    privateField.set(obj,Float.parseFloat(String.valueOf(value)));
+                } else if (type.equals("class java.math.BigDecimal")){
+                    privateField.set(obj,new BigDecimal(String.valueOf(value)));
+                } else if (type.equals("class java.util.Date")) {
+                    if (value.toString().length() == 10) {
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        Date utilDate =sdf.parse(String.valueOf(value));
+                        privateField.set(obj, utilDate);
+                    }else  {
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        Date utilDate =sdf.parse(String.valueOf(value));
+                        privateField.set(obj, utilDate);
+                    }
+                }
+                //可继续追加类型
+            }
+        }
+        return obj;
     }
+    /*拿到反射父类私有属性*/
+    private static Field getPrivateField(String name, Class cls) {
+        Field declaredField = null;
+        try {
+            declaredField = cls.getDeclaredField(name);
+        } catch (NoSuchFieldException ex) {
+
+            if (cls.getSuperclass() == null) {
+                return declaredField;
+            } else {
+                declaredField = getPrivateField(name, cls.getSuperclass());
+            }
+        }
+        return declaredField;
+    }
+
+
+    /**
+     * @Title:transMap2Bean2
+     * @Description:Map转bean
+     * @param:[map, obj]
+     * @return:void
+     * @author: rmk
+     */
+    public static void transMap2Bean2(Map<String, Object> map, Object obj) throws InvocationTargetException, IllegalAccessException {
+
+        if (map == null || obj == null) {
+            return;
+        }
+        try {
+            BeanUtils.populate(obj, map);
+        } catch (Exception e) {
+            System.out.println("transMap2Bean2 Error " + e);
+            throw e;
+        }
+    }
+
+    /**
+     * @Title:transBean2Map
+     * @Description:bean转map
+     * @param:[obj]
+     * @return:java.util.Map<java.lang.String,java.lang.Object>
+     * @author: rmk
+     */
+    public static Map<String, Object> transBean2Map(Object obj) {
+
+        if (obj == null) {
+            return null;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor property : propertyDescriptors) {
+                String key = property.getName();
+
+                // 过滤class属性
+                if (!key.equals("class")) {
+                    // 得到property对应的getter方法
+                    Method getter = property.getReadMethod();
+                    Object value = getter.invoke(obj);
+
+                    map.put(key, value);
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("transBean2Map Error " + e);
+        }
+
+        return map;
+
+    }
+
+
+
 }
---
+++ src/main/java/com/laoxu/tools/StringUtil.java
@@ -0,0 +1,94 @@
+package com.laoxu.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * String 操作类
+ */
+public class StringUtil {
+
+    /**
+     * 首字母大写
+     * @param str
+     * @return
+     */
+    public static String firstCharUp (String str) {
+        char[] ch = str.toCharArray();
+        if (ch[0] >= 'a' && ch[0] <= 'z') {
+            ch[0] = (char) (ch[0] - 32);
+        }
+        return new String(ch);
+    }
+
+    /**
+     * string 转换成Intger数组
+     *
+     * @param str
+     * @param splitStr
+     * @return
+     */
+    public final static List<Integer> toIntegerList(String str, String splitStr) {
+        if (str != null) {
+            ArrayList<Integer> integerList = new ArrayList<Integer>();
+            String[] strList = str.split(splitStr);
+            for (String string : strList) {
+                integerList.add(Integer.parseInt(string));
+            }
+            return integerList;
+        }
+        return null;
+    }
+
+    /**
+     * 功能描述： 反转   右边数字移动到左边，左边的下标往右边 [1,2,5,7,10]  传入数组 , len =2   [7,10,1,2,5]
+     * 修改人：xdc
+     * 创建时间：2019-05-10 9:48
+     * @param a  数组
+     * @param len  须要从第几位开始
+     */
+    public static int[]  reverseArr (int[] a,int len) {
+        int[] newArr = new int[a.length];
+        int[] houArr = new int[len];
+        for (int i =0; i < a.length; i ++) {
+            if ( i < a.length-len) {
+                newArr[i+len]=a[i];
+            } else {
+                houArr[a.length-i-1]=a[i];
+            }
+        }
+        a=newArr.clone();
+        int[] arr = houArr.clone();
+        for(int i=0;i<arr.length ;i++) {
+            a[arr.length-i-1] = arr[i];
+        }
+        return a;
+    }
+
+
+
+    /**
+     * 生成一个Oracle数据库格式的GUID
+     *
+     * @return String
+     */
+    public static String getOracleID() {
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase();
+    }
+    /**
+     * 生成一个SQLServer数据库格式的GUID
+     *
+     * @return String
+     */
+    public static String getSQLServerID() {
+        return UUID.randomUUID().toString().toUpperCase();
+    }
+
+    public static String getMysqlID() {
+        return getOracleID();
+    }
+
+
+
+}
